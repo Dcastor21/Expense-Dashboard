6abc83f5a36dede1999e57bdadd1b9bb56bfc33e
Darnel Castor <darnelcastor2@gmail.com>
2024-10-04 21:43:44 -0400

    update gitignore and settings.py

--- pyexpensewebsite/settings.py
+++ pyexpensewebsite/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
+    'whitenoise.runserver_nostatic', 
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
