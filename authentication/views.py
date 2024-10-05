421bcae187273de9e46f02817ee4d4c6cc31360c
Darnel Castor <darnelcastor2@gmail.com>
2024-10-05 12:41:21 -0400

    Ajax Username Validation part 1

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.views import View
-
+import json
+from django.http import JsonResponse
 # Create your views here.
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        
+        if not str(username).isalnum():
+            return JsonResponse({'username': 'Username should only contain alphanumeric characters'}, status=400)
+        return JsonResponse({'username_vaild': True})
 
 class RegistrationView(View):
     def get(self, request):
